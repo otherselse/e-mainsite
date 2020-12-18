609469c7c3688328381069a73a55b445c8ed5340
xyy <otherselse@163.com>
2020-12-18 17:47:43 +0800

    a

---
+++ js/footer.js
@@ -0,0 +1,43 @@
+    (function(){
+  var str1='<div class="footer m1366 ovh">'+
+      '  <div class="footer-main w1180 tc auto">'+
+      '      <div class="footer-dl l cl mt50 tl">'+
+      '          <dl class="l f14 mr130 ">'+
+      '              <dt class="f18 wh h30 lh30 mb15">Agreement</dt>'+
+	    '            <dd class="c859 h30 lh30 footer-dd"><a href="/english/login/provision.jsp" class="c859" target="_blank">Registration Agreement</a> </dd>'+
+	    '            <dd class="c859 h30 lh30 footer-dd"><a href="/english/login/enxshetong.jsp" class="c859" target="_blank">Sales Contract</a> </dd>'+
+	    '            <dd class="c859 h30 lh30 footer-dd "><a href="/english/login/advertContract.jsp" class="c859" target="_blank"> Advertising Cooperation</a></dd>'+
+	    '            <dd class="c859 h30 lh30"><a href="/english/login/endcht.jsp" class="c859" target="_blank">Booking Space Contract</a> </dd>'+
+      '        </dl>'+
+      '          <dl class="l f14 mr116">'+
+      '             <dt class="f18 wh h30 lh30 mb15">Links</dt>'+
+	    '            <dd class="c859 h30 lh30 footer-dd"><a href="http://www.lgsteel.com/" class="c859" target="_blank">lgsteel.com</a> </dd>'+
+	    '            <dd class="c859 h30 lh30 footer-dd"><a href="http://www.hkhuaye.com/" class="c859" target="_blank">hkhuaye.com</a> </dd>'+
+	    '            <dd class="c859 h30 lh30 footer-dd"><a href="http://www.lgsteel.en.alibaba.com/" class="c859" target="_blank">lgsteel.en.alibaba.com</a> </dd>'+
+      '        </dl>'+
+      '          <dl class="l f14 ">'+
+      '              <dt class="f18 wh h30 lh30 mb15">Contact us</dt>'+
+	    '            <dd class="c859 h30 lh30">No.288 JINLE Road,Baoshan District,Shanghai,China</dd>'+
+	    '            <dd class="c859 h30 lh30">Email: info@lg-steel.com</dd>'+
+	    '            <dd class="c859 h30 lh30">Phone: +0086-4008-200-666</dd>'+
+	    '            <dd class="c859 h30 lh30">Booking Space Contract</dd>'+
+      '          </dl>'+
+      '      </div>'+
+      '  </div>'+
+    '</div>'+
+    '<div class="bottom m1366 ovh">'+
+    '    <div class="bottom-main w1180 tl auto ovh cl mt20">'+
+    '        <div class="bottom-l l c859 f14">'+
+    '          <span>  CopyRight(C)2013-2020 Shanghai Legang Supply Chain Co.,Ltd.Registered <a href="http://beian.miit.gov.cn" class="c859">NO. Hu ICP 14009900</a> registered</span>'+
+    '        <span> Shanghai B2-20160002 value-added telecommunications business license</span>'+
+    '       </div>'+
+    '      <div class="bottom-r r">'+
+    '        <a href="https://www.facebook.com/" class="icon-faceb" target="_blank"></a>'+
+    '        <a href="https://twitter.com" class="icon-twi" target="_blank"></a>'+
+    '        <a href="https://www.linkedin.com" class="icon-in" target="_blank"></a>'+
+    '        <a href="http://www.wechat.com/en/" class="icon-wechat" target="_blank"></a>'+
+    '       </div>'+
+    '    </div>'+
+    '</div>'
+    document.write(str1);
+})()
